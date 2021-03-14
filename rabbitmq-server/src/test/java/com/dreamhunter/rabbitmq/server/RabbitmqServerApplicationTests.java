8f5bdd3e139db48036350680ef41e25fa56d04d3
jinmu333 <jinmu333@qq.com>
2021-03-14 23:18:12 +0800

    feat: init

---
+++ rabbitmq-server/src/test/java/com/dreamhunter/rabbitmq/server/RabbitmqServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamhunter.rabbitmq.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
